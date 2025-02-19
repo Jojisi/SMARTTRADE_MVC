5f1f7c21c20fe7952056e0b608e9e929a3e6e0de
“joan” <joanjimenez912@gmail.com>
2025-02-20 00:08:03 +0100

    product-list_shop

--- module/shop/model/DAO_shop.php
+++ module/shop/model/DAO_shop.php
@@ -3,23 +3,22 @@ $path = $_SERVER['DOCUMENT_ROOT'] . '/SMARTTRADE_MVC/';
 include($path . "/model/connect.php");
 
 class DAOShop{
-	function select_all_products() {
-		$sql = "SELECT p.id_product, p.name, p.price, p.img_product, c.name_condition_type, cat.name AS category, ci.name_city 
-				FROM product p
-				INNER JOIN conditiontype c ON p.condition_id = c.id_condition_type
-				INNER JOIN catalog cat ON p.category_id = cat.id_catalog
-				INNER JOIN city ci ON p.city_id = ci.id_city";
-	
-		$conexion = connect::con();
-		$res = mysqli_query($conexion, $sql);
-		connect::close($conexion);
-	
-		$retrArray = array();
-		if (mysqli_num_rows($res) > 0) {
-			while ($row = mysqli_fetch_assoc($res)) {
-				$retrArray[] = $row;
-			}
-		}
-		return $retrArray;
-	}
+    function select_all_products() {
+        $sql = "SELECT p.id_product, p.name, p.price, p.img_product, p.visit_count, c.name_condition_type, s.status_state
+                FROM product p
+                INNER JOIN conditiontype c ON p.condition_id = c.id_condition_type
+                INNER JOIN salestatus s ON p.sale_status_id = s.id_sale_status";
+
+        $conexion = connect::con();
+        $res = mysqli_query($conexion, $sql);
+        connect::close($conexion);
+
+        $retrArray = array();
+        if (mysqli_num_rows($res) > 0) {
+            while ($row = mysqli_fetch_assoc($res)) {
+                $retrArray[] = $row;
+            }
+        }
+        return $retrArray;
+    }
 }
--- module/shop/model/controller_shop.js
+++ module/shop/model/controller_shop.js
@@ -3,7 +3,7 @@ function loadProducts() {
 }
 
 function ajaxForSearch(url) {
-    ajaxPromise(url, 'GET', 'JSON')  // Primero va la URL, luego GET y JSON
+    ajaxPromise(url, 'GET', 'JSON')
         .then(function (data) {
             console.log(data);
             $('#content_shop_products').empty();
@@ -15,26 +15,17 @@ function ajaxForSearch(url) {
                 for (let row in data) {
                     $('<div></div>').attr({ 'id': data[row].id_product, 'class': 'list_content_shop col-md-7' }).appendTo('#content_shop_products')
                         .html(
-                            "<div class='card card-product' style='background-color: #f2f2f2;'>" +
-                            "<div class='col-md-1'></div>" +
-                            "<img src='" + data[row].img_product + "' class='card-img-top'>" +
-                            "<div class='card-body'>" +
-                            "<div class='product-info'>" +
-                            "<div class='product-content'>" +
-                            "<h1><b>" + data[row].name + "</b></h1>" +
-                            "<p><strong>Precio:</strong> " + data[row].price + '€</p>' +
-                            "<ul>" +
-                            // "<li> <i class='fa-solid fa-location-dot fa-xl'></i>&nbsp;&nbsp;" + data[row].location + "</li>" +
-                            "<li> <i class='fa-solid fa-file-lines fa-xl'></i>&nbsp;&nbsp;" + data[row].description + "</li>" +
-                            // "<li> <i class='fa-solid fa-calendar fa-xl'></i>&nbsp;&nbsp;" + data[row].publication_date + "</li>" +
-                            "</ul>" +
-                            "<div class='buttons'>" +
-                            "<button id='btn_" + data[row].id_product + "' class='btn btn-success'>Comprar</button>" +
-                            "</div>" +
-                            "</div>" +
-                            "</div>" +
-                            "</div>" +
-                            "</div>"
+                            `<div class='card card-product' style='background-color: #f2f2f2;'>
+                                <img src='${data[row].img_product}' class='card-img-top'>
+                                <div class='card-body'>
+                                    <h1><b>${data[row].name}</b></h1>
+                                    <p><strong>Price:</strong> ${data[row].price}€</p>
+                                    <p><strong>Condition:</strong> ${data[row].name_condition_type}</p>
+                                    <p><strong>Sale status:</strong> ${data[row].status_state}</p>
+                                    <p><strong>Visit count:</strong> ${data[row].visit_count}</p>
+                                    <button id='btn_${data[row].id_product}'>Buy NOW</button>
+                                </div>
+                            </div>`
                         );
                 }
             }
